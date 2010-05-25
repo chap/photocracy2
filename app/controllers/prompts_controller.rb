4018bdd8bc7f36f6f2f64e4f493a4c440b72b5fe
Chap Ambrose <chapambrose@gmail.com>
2010-05-25 17:55:40 -0400

    Basic voting functionality working.

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,37 +1,48 @@
 class PromptsController < ActionController::Base
   def vote
-    @prompt = Prompt.find(params[:id], :params => {:question_id => params[:question_id]})
+    voted_prompt = Prompt.find(params[:id], :params => {:question_id => params[:question_id]})
     
     if params[:direction] &&
-       vote = @prompt.post("vote_#{params[:direction]}".to_sym,
-                           :question_id => params[:question_id],
-                           :format => :json,
-                           # have to nest this because of 
-                           # prompts#vote_direction in pairwise
-                           :params => {
-                             :auto => request.session_options[:id], 
-                             :time_viewed => "5936", 
-                             :appearance_lookup => "0e0d7325aadff0110725fa46c91129be"
-                           }
-                          )
-		  puts vote
-		  # "params"=>{"appearance_lookup"=>"0e0d7325aadff0110725fa46c91129be", "time_viewed"=>"5936", "auto"=>"6ad13dbd516c3256b3dcc185dc76a4a6"}
-      # newprompt = Crack::XML.parse(p.body)['prompt']
-      # 
-      # leveling_message = Visitor.leveling_message(:votes => newprompt['visitor_votes'].to_i,
-      # :ideas => newprompt['visitor_ideas'].to_i)
-      # 
-      # logger.info "newprompt is #{newprompt.inspect}"
-      # render :json => {:votes => 20, :newleft => truncate((newprompt['left_choice_text']), :length => 137), 
-      # :newright => truncate((newprompt['right_choice_text']), :length => 137), 
-      # :leveling_message => leveling_message,
-      # :prompt_id => newprompt['id'],
-      # :appearance_lookup => newprompt['appearance_id']
-      # }.to_json
-      render :text => "it worked"
+      vote = voted_prompt.post("vote_#{params[:direction]}".to_sym,
+        :question_id => params[:question_id],
+        :params => {
+          :auto => request.session_options[:id],
+          :time_viewed => params[:time_viewed],
+          :appearance_lookup => params[:appearance_lookup]
+        }
+      )
+
+      next_prompt = Crack::XML.parse(vote.body)['prompt']
+      logger.info(next_prompt.inspect)
+      render :json => {
+        :right_choice_text => next_prompt['right_choice_text'],
+        :left_choice_text  => next_prompt['left_choice_text'],
+        :right_choice_id   => next_prompt['right_choice_id'],
+        :left_choice_id    => next_prompt['left_choice_id'],
+        :visitor_votes     => next_prompt['visitor_votes'],
+        :appearance_lookup => next_prompt['appearance_id'],
+        :prompt_id         => next_prompt['id'],
+        :question_id       => params[:question_id],
+        :show_prompt_path  => question_prompt_path(params[:question_id], next_prompt['id'])
+      }.to_json
     else
-      #render :json => vote.errors, :status => :unprocessable_entity
-      render :text => 'error', :status => :unprocessable_entity
+      render :text => 'Vote unsuccessful.', :status => :unprocessable_entity
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.js {
+        @right_choice_photo = Photo.find(params['right_choice_text'])
+        @left_choice_photo = Photo.find(params['left_choice_text'])
+        @left_choice_id = params['left_choice_id']
+        @right_choice_id = params['left_choice_id']
+
+        @prompt = Prompt.find(params['id'], :params => {:question_id => params[:question_id]})
+        @question = Question.find(params[:question_id])
+
+        render :partial => 'questions/prompt'
+      }
     end
   end
 end
