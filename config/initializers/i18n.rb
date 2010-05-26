b70f8dc5584cc40bcee3a7293a29654ef488bdf4
Chap Ambrose <chapambrose@gmail.com>
2010-05-26 18:34:44 -0400

    comment out I18n initializers

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,2 +1,2 @@
-I18n.backend.class.send(:include, I18n::Backend::Fallbacks)
-I18n.fallbacks.map('fr' => 'en')
+# I18n.backend.class.send(:include, I18n::Backend::Fallbacks)
+# I18n.fallbacks.map('fr' => 'en')
